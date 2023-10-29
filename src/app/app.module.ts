b1eb087c6ac4c5f7c6fe2bdac9f1004a70ea272a
Cida Luna <cidalunna@hotmail.com>
2023-10-29 16:05:37 -0300

    Add material lib and separeted detail and list component with decorator Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes/heroes.component.ts
+++ src/app/heroes/heroes/heroes.component.ts
@@ -8,11 +8,6 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: '1',
-    name: 'Wolverine'
-  } ;
-
   heroes = HEROES;
   selectedHero?: Hero;
 
