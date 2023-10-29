398003293694418613bca6aae57e3c808fc2d4ef
Cida Luna <cidalunna@hotmail.com>
2023-10-29 16:51:36 -0300

    Simulated throwError

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero.model';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +11,8 @@ export class HeroService {
   getHero(): Observable<Hero[]>{ // retorna o Observable de uma lista de Hero[]
     //return HEROES; // retorno lista de heroes mock, dados sincronos, estão aqui, no mesmo momento que eu peço os dados eu obtenho
     const heroes = of(HEROES); // transforma o mock no Observable
+
+    //return throwError(new Error('Ocorreu um problema'));
     return heroes;
   }
 }
