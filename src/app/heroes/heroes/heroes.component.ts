cff3f5db76d04f36348aaa7aeca5e6f4dea0a00e
Cida Luna <cidalunna@hotmail.com>
2023-10-29 13:13:09 -0300

    Implemented list of heroes mock

--- src/app/heroes/heroes/heroes.component.ts
+++ src/app/heroes/heroes/heroes.component.ts
@@ -1,20 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
   hero: Hero = {
     id: '1',
     name: 'Wolverine'
   } ;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  heroes = HEROES;
 
 }
