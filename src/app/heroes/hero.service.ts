da2644379ee6ff2637fc4568a3f232046006eca0
Cida Luna <cidalunna@hotmail.com>
2023-10-29 21:36:27 -0300

    implemented clear messages

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero.model';
 import { Observable, of, throwError } from 'rxjs';
+import { MessageService } from '../messages/message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  constructor(private messageService: MessageService){}
+
   getHero(): Observable<Hero[]>{ // retorna o Observable de uma lista de Hero[]
     //return HEROES; // retorno lista de heroes mock, dados sincronos, estão aqui, no mesmo momento que eu peço os dados eu obtenho
     const heroes = of(HEROES); // transforma o mock no Observable
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 }
