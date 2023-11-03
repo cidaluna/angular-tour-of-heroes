fde9e8be075e7d447caee135361ea47a8d210f9f
Cida Luna <cidalunna@hotmail.com>
2023-11-03 11:40:32 -0300

    Created a custom Materail Module in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,5 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +7,7 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes/heroes.component';
+import { MaterialModule } from './material/material.module';
 import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
@@ -30,15 +22,7 @@ import { MessagesComponent } from './messages/messages.component';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatExpansionModule,
-    MatIconModule,
-    MatTooltipModule,
-    MatCardModule,
-    MatListModule,
-    MatInputModule,
-    MatTableModule,
+    MaterialModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+const MODULES = [MatToolbarModule,
+  MatButtonModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatTooltipModule,
+  MatCardModule,
+  MatListModule,
+  MatInputModule,
+  MatTableModule]
+@NgModule({
+  declarations: [],
+  imports: [MODULES],
+  exports: [MODULES]
+})
+export class MaterialModule { }
