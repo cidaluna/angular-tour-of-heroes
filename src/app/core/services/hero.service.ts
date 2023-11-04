c77a66d92f478f56f3f613e129971b1a9e152234
Cida Luna <cidalunna@hotmail.com>
2023-11-03 23:12:31 -0300

    Added proxy conf json and rewrite hero service

--- src/app/core/services/hero.service.ts
+++ src/app/core/services/hero.service.ts
@@ -1,16 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Hero } from '../models/hero.model';
 import { MessageService } from './message.service';
-import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';
 
-  constructor(private messageService: MessageService){}
+  constructor(private http: HttpClient, private messageService: MessageService){}
 
+  /*
   getHero(): Observable<Hero[]>{ // retorna o Observable de uma lista de Hero[]
     //return HEROES; // retorno lista de heroes mock, dados sincronos, estão aqui, no mesmo momento que eu peço os dados eu obtenho
     const heroes = of(HEROES); // transforma o mock no Observable
@@ -22,5 +25,28 @@ export class HeroService {
     const heroId = HEROES.find(resp => Number(resp.id) === id)!;
     this.messageService.add(`HeroService: fetched hero id = ${id}`);
     return of(heroId);
+  }
+  */
+
+  // GET /heroes
+  getHero(): Observable<Hero[]>{
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((resp) => {
+          this.log(`fetched ${resp.length} hero(es).`)
+        }
+      ))
+  }
+
+  // GET /heroes/id
+  getHeroId(id: number): Observable<Hero>{
+    return this.http.get<Hero>(`${this.heroesUrl}/${id}`).pipe(
+      tap((resp) => {
+        this.log(`fetched hero id=${id} and name=${resp.name}`)
+      }
+    ))
+  }
+
+  private log(message: string): void{
+    this.messageService.add(`Hero service: ${message}`)
   }
 }
